34e3f94fd22c63f1492ecd40578dd0574e46ca45
palomara <palomarangelll@hotmail.com>
2018-05-18 20:47:34 -0300

    delete and update - not done

--- aulathrows/src/view/Arquivo.java
+++ aulathrows/src/view/Arquivo.java
@@ -5,12 +5,13 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.StringTokenizer;
 
 public class Arquivo {
 
 	public void inserir(String texto) throws IOException {
 		//Gravação 
-		FileWriter arq = new FileWriter("d:\\teste.txt");
+		FileWriter arq = new FileWriter("d:\\teste.txt", true);
 		PrintWriter gravarArq = new PrintWriter(arq);
 		
 		gravarArq.println(texto);
@@ -36,8 +37,40 @@ public class Arquivo {
 			}
 		 arqleitura.close();
 		}
+	public void update (String texto, String novoTexto) throws IOException  {
+		
+		//Leitura
+		FileReader arqLeitura = new FileReader("d:\\teste.txt");
+		BufferedReader lerArq = new BufferedReader(arqLeitura);
+		FileWriter arq = new FileWriter("d:\\teste.txt", true);
+		PrintWriter gravarArq = new PrintWriter(arq);
+		
+		String linha = lerArq.readLine();
+		int i = 0;
+		String textOld = "";
+		 while (linha != null) {
+			 i++;
+			 if (linha.contains(texto)) {
+				 textOld += novoTexto + "\n";
+				
+			 }
+			 else {
+				 textOld += linha + "\n";
+			 }
+			 gravarArq.println(novoTexto);
+			 linha = lerArq.readLine();
+		 }
+		 
+		 System.out.println(textOld);
+		 arqLeitura.close();
+	}
+		 
+
 	public static void main (String args[]) throws Exception {
 		Arquivo arq = new Arquivo();
-		arq.inserir("testando...");
+		//arq.inserir("testando3...");
+		//arq.leitura("testando2...");
+		arq.update("testando...", "testando...2");
+		
 	}
 }
